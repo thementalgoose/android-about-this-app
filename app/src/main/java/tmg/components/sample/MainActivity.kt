ebf475955b80479c9b129d6418cdadb34bc5dadc
Jordan Fisher <thementalgoose@gmail.com>
2020-05-17 16:33:54 +0100

    Fixed dark mode support in Material Components

--- app/src/main/java/tmg/components/sample/MainActivity.kt
+++ app/src/main/java/tmg/components/sample/MainActivity.kt
@@ -43,6 +43,37 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
+        btnAboutThisAppDarkMode.setOnClickListener {
+            startActivity(
+                AboutThisAppActivity
+                    .intent(
+                        context = this,
+                        isDarkMode = true,
+                        appName = "Sample App",
+                        appVersion = "1.0.0",
+                        dependencies = List(4) {
+                            AboutThisAppDependency(
+                                order = it,
+                                dependencyName = "Utilities",
+                                author = "Jordan Fisher",
+                                imageUrl = "https://avatars0.githubusercontent.com/u/5982159?s=460&v=4",
+                                url = "https://github.com/thementalgoose/android-utilities"
+                            )
+                        },
+                        email = "thementalgoose@gmail.com",
+                        footnote = "Thank you!",
+                        github = "https://github.com/thementalgoose/android-components",
+                        play = null,
+                        name = "Jordan Fisher",
+                        nameDesc = "App developer!",
+                        imageUrl = "https://avatars0.githubusercontent.com/u/5982159?s=460&v=4",
+                        packageName = this.packageName,
+                        thankYou = "Thank you!",
+                        website = "https://jordanfisher.io"
+                    )
+            )
+        }
+
         btnSettings.setOnClickListener {
             startActivity(Intent(this, SettingsActivity::class.java))
         }
